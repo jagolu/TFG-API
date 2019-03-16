3dbdf5bf580769b5ba1c8ca591d371fd3fd45987
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-16 19:41:05 +0100

    Add EmailVerificationController 1.0.0

---
+++ Controllers/Identity/EmailVerificationController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Identity
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailVerificationController : ControllerBase
+    {
+        private Data.ApplicationDBContext _context;
+
+        public EmailVerificationController(Data.ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/EmailVerification
+        [HttpPost]
+        public IActionResult Post([FromBody] object token)
+        {
+            //Convierte el object que se pasa en tipo JSON a tipo string
+            String t = ((dynamic)JObject.Parse(token.ToString())).token;
+
+            var user = _context.User.Where(u => u.tokenValidation == t);
+
+            if (user.Count() != 1) {
+                return BadRequest(new { error = "TokenNotExists" });
+            }
+
+            try {
+                Models.User userUpdate = user.First();
+                userUpdate.tokenValidation = null; 
+
+                _context.User.Update(userUpdate);
+
+                _context.SaveChanges();
+
+                return Ok(new { OK = "success" });
+
+            }catch(Exception e) {
+                return StatusCode(500);
+            }
+        }
+    }
+}
