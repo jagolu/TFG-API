efa22c551ae9ea93b4a2b8a08f5f5deb1d031e1b
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-21 23:36:33 +0100

    Update AuthenticationController
    Added:
       - logIn
       - validateEmail
    Update:
       - Refresh
    Started:
       - VerifyFacebookToken

--- Controllers/Identity/AuthenticationController.cs
+++ Controllers/Identity/AuthenticationController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
@@ -23,19 +24,19 @@ namespace API.Controllers.Identity
     public class AuthenticationController : ControllerBase
     {
         private Data.ApplicationDBContext _context;
-        private IConfiguration config;
+        private IConfiguration _config;
 
-        public AuthenticationController(Data.ApplicationDBContext c, IConfiguration conf)
+        public AuthenticationController(Data.ApplicationDBContext c, 
+                                        IConfiguration conf)
         {
             this._context = c;
-            this.config = conf;
+            this._config = conf;
         }
         
-        // POST: api/Authentication
         [HttpPost]
         [AllowAnonymous]
         [ActionName("SignUp")]
-        public IActionResult signUp([FromBody] UserRequest user)
+        public IActionResult signUp([FromBody] UserSignUp user)
         {
             /*if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
@@ -50,20 +51,17 @@ namespace API.Controllers.Identity
                 User u = new User {
                     email = user.email,
                     nickname = user.username,
-                    password = user.getHashPassword(this.config),
+                    password = user.getHashPassword(this._config),
                     tokenValidation = (user.password == null) ? null : Guid.NewGuid().ToString("N")
                 };
                 this._context.User.Add(u);
 
-                Email email = new Email(this.config);
+                Email email = new Email(this._config);
                 email.sendVerificationToken(u.email, u.nickname, u.tokenValidation);
 
                 this._context.SaveChanges();
 
-                string refreshToken = this.generateRefreshToken(u.email, user.provider);
-                string token = this.generateToken(u.email, refreshToken);
-
-                return Ok(new { token = token });
+                return Ok();
 
             } catch (Exception e) {
                 return StatusCode(500);
@@ -72,32 +70,93 @@ namespace API.Controllers.Identity
 
         [HttpPost]
         [AllowAnonymous]
+        [ActionName("LogIn")]
+        public IActionResult logIn([FromBody] UserLogIn user)
+        {
+            var userExist = this._context.User.Where(u => u.email == user.email);
+            
+            if( (userExist.Count() != 1) || (user.getHashPassword(this._config) != userExist.First().password) ) {
+                return BadRequest(new { error = "WrongEmailOrPassword" });
+            }
+
+            if((userExist.First().tokenValidation != null)) {
+                return BadRequest(new { error = "NotValidatedYet" });
+            }
+
+            string refreshToken = this.generateRefreshToken(user.email, user.provider);
+            string token = this.generateToken(user.email, refreshToken);
+
+            return Ok(new { token = token });
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ActionName("Validate")]
+        public IActionResult validateEmail(string emailToken, Boolean provider = false)
+        {
+            var tokenExists = this._context.User.Where(u => u.tokenValidation == emailToken);
+
+            if(tokenExists.Count() != 1) {
+                return BadRequest(new { error = "NonExistingToken" });
+            }
+
+            User user = tokenExists.First();
+
+            try {
+                user.tokenValidation = null;
+
+                this._context.Update(user);
+                this._context.SaveChanges();
+
+            }catch(Exception e) {
+                return StatusCode(500);
+            }
+
+            string refreshToken = this.generateRefreshToken(user.email, provider);
+            string token = this.generateToken(user.email, refreshToken);
+
+            return Ok(new { token = token });
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ActionName("SocialLog")]
+        public IActionResult socialLog(string tok) {
+            string b = tok;
+            Boolean isOk = this.verifyFacebookTokenAsync(tok).;
+            if (isOk) {
+                return BadRequest(new {error="TokenDoesntExists");
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
         [ActionName("Refresh")]
-        public IActionResult Refresh([FromBody] refreshRequest req)
+        public IActionResult refresh([FromBody] refreshRequest req)
         {
             var principal = this.getPrincipalFromExpiredToken(req.token);
 
             string email = principal.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
             string refreshToken = principal.FindFirst("refreshToken").Value;
             var savedRefreshToken = this._context.UserToken.Where(ut => ut.refreshToken == refreshToken);
-            
-            if(savedRefreshToken.Count() != 1) {
+
+            if (savedRefreshToken.Count() != 1) {
                 throw new SecurityTokenException("Invalid refresh token");
             }
 
             string newRefreshToken = this.generateRefreshToken(email, req.provider);
             string newToken = this.generateToken(email, newRefreshToken);
 
-            return Ok(new { token=newToken });
+            return Ok(new { token = newToken });
         }
 
         private String generateToken(string email, string refreshToken)
         {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.config["Jwt:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this._config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
-                issuer: this.config["Jwt:Issuer"],
+                issuer: this._config["Jwt:Issuer"],
                 claims: new Claim[] {
                     new Claim(ClaimTypes.Email, email),
                     new Claim("refreshToken", refreshToken)
@@ -145,7 +204,7 @@ namespace API.Controllers.Identity
             {
                 ValidateAudience = false,
                 ValidateIssuer = false,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"])),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])),
                 ValidateLifetime = false
             };
 
@@ -161,9 +220,29 @@ namespace API.Controllers.Identity
 
             return principal;
         }
+
+        private async Task<bool> verifyFacebookTokenAsync(string token)
+        {
+            string facebookId = this._config["Social:facebook"];
+            /*var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://graph.facebook.com/debug_token?" +
+                "input_token=" + token +
+                "&access_token=" + facebookId);*/
+
+            var request = "https://graph.facebook.com/debug_token?" +
+                "input_token=" + token +
+                "&access_token=" + facebookId;
+
+            var client = new HttpClient();
+
+            var response = await client.GetStringAsync(request);
+
+            //TODO get body of the response
+            return false;
+        }
     }
 
-    public class UserRequest
+    public class UserSignUp
     {
         [Required]
         [EmailAddress(ErrorMessage = "This is not a valid email")]
@@ -173,6 +252,33 @@ namespace API.Controllers.Identity
         [MinLength(4, ErrorMessage = "Username must have at least 3 characters")]
         [MaxLength(20, ErrorMessage = "Username must have less than 20 characters")]
         public string username { get; set; }
+
+        [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
+        [MaxLength(20, ErrorMessage = "Password must have less than 20 characters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{1,}$",
+            ErrorMessage = "Password must have at least a lowercase, a uppercase and a number")]
+        public string password { get; set; }
+
+        public Boolean provider { get; set; } = false;
+
+        public string getHashPassword(IConfiguration configuration)
+        {
+            if (password == null) return null;
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: new byte[int.Parse(configuration["Crypt:saltSize"])],
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: int.Parse(configuration["Crypt:hashCount"]),
+                numBytesRequested: int.Parse(configuration["Crypt:subkeyLength"])
+            )); ;
+        }
+    }
+
+    public class UserLogIn
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid email")]
+        public string email { get; set; }
 
         [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
         [MaxLength(20, ErrorMessage = "Password must have less than 20 characters")]
