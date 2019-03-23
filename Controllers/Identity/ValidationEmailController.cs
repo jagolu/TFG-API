2a00df8b3c477b772d5855a81ed136b370febaa4
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-23 13:30:50 +0100

    Create ValidationEmailController.cs

---
+++ Controllers/Identity/ValidationEmailController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Controllers.Identity
+{
+    [Route("Authorization/[action]")]
+    [ApiController]
+    public class ValidationEmailController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+        private IConfiguration _configuration;
+
+        public ValidationEmailController(ApplicationDBContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ActionName("Validate")]
+        public IActionResult validateEmail([Required] string emailToken, Boolean provider = false)
+        {
+            var tokenExists = _context.User.Where(u => u.tokenValidation == emailToken);
+
+            if (tokenExists.Count() != 1) {
+                return BadRequest(new { error = "NonExistingToken" });
+            }
+
+            User user = tokenExists.First();
+            
+            user.tokenValidation = null;
+
+            _context.Update(user);
+
+            try {
+                string newRefreshToken = TokenGenerator.generateRefreshToken(_context, user.email, provider);
+                string newToken = TokenGenerator.generateToken(user.email, newRefreshToken);
+
+                _context.SaveChanges();
+
+                return Ok(new { token = newToken });
+
+            } catch (DbUpdateException) {
+                return StatusCode(500);
+
+            } catch (Exception) {
+                return BadRequest(new { error = "InvalidToken" });
+
+            }
+        }
+    }
+}
