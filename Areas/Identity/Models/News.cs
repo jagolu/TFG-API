e756db2c5e5b04e22fa65237663ebffa417cc5e1
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-27 00:12:19 +0100

    Add databaseModel ---> News

---
+++ Areas/Identity/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Areas.Identity.Models
+{
+    public class News
+    {
+        public Guid groupId { get; set; } //PrimaryKey
+        public Guid userId { get; set; } //PrimaryKey
+        public DateTime time { get; set; } = DateTime.Now; //PrimaryKey
+
+        [Required]
+        [MaxLength(30)]
+        public string title { get; set; }
+
+        [Required]
+        [MaxLength]
+        public string body { get; set; }
+    }
+}
