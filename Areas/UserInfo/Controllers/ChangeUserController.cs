2840aaf1f7b42d3602490b8312a671015c33125d
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-31 00:03:04 +0100

    Add ChnageUserController

---
+++ Areas/UserInfo/Controllers/ChangeUserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using API.Areas.UserInfo.Models;
+using API.Data;
+using API.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.UserInfo.Controllers
+{
+
+    [Route("User/[action]")]
+    [ApiController]
+    public class ChangeUserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ChangeUserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ActionName("ChangeUserInfo")]
+        public IActionResult changeUser([FromBody] ChangeUserInfo info)
+        {
+            var authToken = HttpContext.Request?.Headers["Authorization"];
+            string email = TokenGenerator.getEmailClaim(TokenGenerator.getBearerToken(authToken.Value));
+
+            User user = _context.User.Where(u => u.email == email).First();
+
+            user.nickname = info.nickname;
+            user.password = info.password ?? user.password ?? PasswordHasher.hashPassword(info.password);
+            user.profileImg = info.image;
+
+            try {
+                _context.Update(user);
+                _context.SaveChanges();
+
+            } catch (Exception) {
+                return StatusCode(500);
+            }
+
+            return Ok();
+        }
+    }
+}
