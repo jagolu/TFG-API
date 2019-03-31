b72d8625f2b86f9bc3ba11a34adf8fe3626f7686
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-31 01:00:43 +0100

    Update ChangeUserInfo.cs

--- Areas/UserInfo/Models/ChangeUserInfo.cs
+++ Areas/UserInfo/Models/ChangeUserInfo.cs
@@ -7,19 +7,14 @@ namespace API.Areas.UserInfo.Models
 {
     public class ChangeUserInfo
     {
-        [Required]
-        [MinLength(4, ErrorMessage = "Username must have at least 3 characters")]
-        [MaxLength(20, ErrorMessage = "Username must have lesss than 20 characters")]
         public string nickname { get; set; }
 
-        [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
-        [MaxLength(20, ErrorMessage = "Password must have less than 20 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{1,}$",
-            ErrorMessage = "Password must have at least a lowercase, a uppercase and a number")]
-        public string password { get; set; }
+        public string oldpassword { get; set; }
 
-        [Required]
-        [MaxLength]
+        public string newPassword { get; set; }
+
+        public string repeatNewPassword { get; set; }
+        
         [JsonConverter(typeof (ConvertBase64ToBlob))]
         public Byte[] image { get; set; }
     }
