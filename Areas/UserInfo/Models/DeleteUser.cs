7c997b318f6ba1f6dbca9d4b5a9256a0b0b448d7
Javier Gómez Luzón <javo14795@gmail.com>
2019-04-04 00:35:15 +0200

    Update deleteUser

--- Areas/UserInfo/Models/DeleteUser.cs
+++ Areas/UserInfo/Models/DeleteUser.cs
@@ -4,14 +4,8 @@ namespace API.Areas.UserInfo.Models
 {
     public class DeleteUser
     {
-        [EmailAddress(ErrorMessage = "This is not a valid email")]
         public string email { get; set; }
 
-        [Required]
-        [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
-        [MaxLength(20, ErrorMessage = "Password must have less than 20 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{1,}$",
-                ErrorMessage = "Password must have at least a lowercase, a uppercase and a number")]
         public string password { get; set; }
     }
 }
