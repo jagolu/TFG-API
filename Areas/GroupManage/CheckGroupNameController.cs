58c890b796ff1f768d815d0a0ecfe0bedefc58b3
Jagolu <javo14795@gmail.com>
2019-04-12 20:28:41 +0200

    Add CheckGroupName

---
+++ Areas/GroupManage/CheckGroupNameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.GroupManage
+{
+    [Route("Group/[action]")]
+    [ApiController]
+    public class CheckGroupNameController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public CheckGroupNameController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [EnableCors]
+        [ActionName("CheckGroupName")]
+        public bool checkName(string name)
+        {
+            if (name == null)
+            {
+                return false;
+            }
+            
+            int groupWithTheSameName = _context.Group.Where(g => g.name == name).Count();
+
+            if (groupWithTheSameName != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
