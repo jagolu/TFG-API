e5e9c6b86ea2b2c713876944184bbdfd099638fc
Jagolu <javo14795@gmail.com>
2019-04-12 13:44:24 +0200

    Add limitations

---
+++ Models/Limitations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Limitations
+    {
+        [Key]
+        public Guid userId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public int socialGroup { get; set; } = 1;
+
+        [Required]
+        public int virtualGroup { get; set; } = 1;
+    }
+}
