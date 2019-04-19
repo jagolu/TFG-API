7986fe809fcb21f2ac251ac4c5729d3200dd45c4
Javier Gómez Luzón <javo14795@gmail.com>
2019-04-19 12:31:59 +0200

    Add getGroupPage funcionality

---
+++ Areas/GroupManage/Controllers/GroupPageController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Areas.GroupManage.Models;
+using API.Data;
+using API.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.GroupManage.Controllers
+{
+    [Route("Group/[action]")]
+    [ApiController]
+    public class GroupPageController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public GroupPageController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [ActionName("GroupPage")]
+        public IActionResult getGroupPage(string groupName)
+        {
+            User user = TokenUserManager.getUserFromToken(HttpContext, _context);
+
+            var groups = _context.Group.Where(g => g.name == groupName);
+
+            if (groups.Count() != 1)
+            {
+                return BadRequest(new { error = "" });
+            }
+
+            Group group = groups.First();
+            _context.Entry(group).Collection("users").Load();
+
+            if (group.users.Where(u=>u.userId == user.id).Count() != 1)
+            {
+                return BadRequest(new { error = "" });
+            }
+
+            GroupPage page = new GroupPage();
+            page.groupName = group.name;
+            page.groupType = group.type;
+
+            //Change ---- this is for try
+            page.bets = new List<GroupBet>{
+                new GroupBet { betName = "bet1", betBody = "betBody1"},
+                new GroupBet { betName = "bet2", betBody = "betBody2"},
+                new GroupBet { betName = "bet3", betBody = "betBody3"},
+                new GroupBet { betName = "bet4", betBody = "betBody4"},
+                new GroupBet { betName = "bet5", betBody = "betBody5"}
+            };
+
+            List<GroupMember> members = new List<GroupMember>();
+
+            foreach(UserGroup ug in group.users)
+            {
+                _context.Entry(ug).Reference("role").Load();
+                _context.Entry(ug).Reference("User").Load();
+                members.Add(new GroupMember { userName = ug.User.nickname ,role = ug.role.name});
+            }
+
+            page.members = members;
+
+            return Ok(page);
+        }
+    }
+}
