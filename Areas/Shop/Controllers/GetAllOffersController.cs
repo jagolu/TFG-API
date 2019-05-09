b8f7f37d851efab196b475c5810e4672100eff13
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-09 23:17:46 +0200

    Add function to get All the shop offers

---
+++ Areas/Shop/Controllers/GetAllOffersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Areas.Shop.Models;
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.Shop.Controllers
+{
+    [Route("Shop/[action]")]
+    [ApiController]
+    public class GetAllOffersController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public GetAllOffersController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [ActionName("GetAllOffers")]
+        public IActionResult getAllShopOffers()
+        {
+            try
+            {
+                List<OfferShop> offersToShow = new List<OfferShop>();
+
+                _context.ShopOffers.ToList().ForEach(offer =>
+                {
+                    _context.Entry(offer).Reference("type").Load();
+                    offersToShow.Add(new OfferShop
+                    {
+                        productId = offer.id.ToString(),
+                        title = offer.title,
+                        price = offer.price,
+                        description = offer.description,
+                        type = offer.type.name
+                    });
+                });
+
+                return Ok(new { offers = offersToShow });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
