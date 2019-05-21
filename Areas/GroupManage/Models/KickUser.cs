59aac5e98ae982a550473aa4bb5216e3366351e6
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-21 21:20:16 +0200

    Add function to kick a user

---
+++ Areas/GroupManage/Models/KickUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Areas.GroupManage.Models
+{
+    public class KickUser
+    {
+        [Required]
+        public string publicId { get; set; }
+
+        [Required]
+        public string groupName { get; set; }
+    }
+}
