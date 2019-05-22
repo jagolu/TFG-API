0bbf4c9ceaf4577cb15f59543ed63146523ff6f1
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-22 23:17:24 +0200

    Refactor --> On GroupPage --> get the page with an static class

--- Areas/GroupManage/Controllers/GroupPageController.cs
+++ Areas/GroupManage/Controllers/GroupPageController.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using API.Areas.GroupManage.Models;
+﻿using System.Linq;
+using API.Areas.GroupManage.Util;
 using API.Data;
 using API.Data.Models;
 using API.Util;
@@ -36,66 +35,14 @@ namespace API.Areas.GroupManage.Controllers
 
             Group group = groups.First();
             _context.Entry(group).Collection("users").Load();
-            var userInTheGroup = group.users.Where(u => u.userId == user.id);
 
             // If the user doesn't belong to the group
-            if (userInTheGroup.Count() != 1)
+            if (group.users.Where(u => u.userId == user.id).Count() != 1)
             {
                 return BadRequest(new { error = "" });
             }
 
-            //Set the group name and group type
-            _context.Entry(userInTheGroup.First()).Reference("role").Load();
-            GroupPage page = new GroupPage();
-            page.groupName = group.name;
-            page.groupType = group.type;
-            page.role = userInTheGroup.First().role.name;
-
-            //Get the role of the user in the group
-            UserGroup ownUserGroup =  group.users.Where(u => u.userId == user.id).First();
-            _context.Entry(ownUserGroup).Reference("role").Load();
-            string callerUser_role = ownUserGroup.role.name;
-            string role_group_normal = _context.Role.Where(r => r.name == "GROUP_NORMAL").First().name;
-
-
-            //Change ---- this is for try
-            page.bets = new List<GroupBet>{
-                new GroupBet { betName = "bet1", betBody = "betBody1"},
-                new GroupBet { betName = "bet2", betBody = "betBody2"},
-                new GroupBet { betName = "bet3", betBody = "betBody3"},
-                new GroupBet { betName = "bet4", betBody = "betBody4"},
-                new GroupBet { betName = "bet5", betBody = "betBody5"}
-            };
-
-            // Set the users who belongs to the group
-            page.members = new List<GroupMember>();
-
-            foreach(UserGroup ug in group.users.OrderBy(ug => ug.dateJoin)) //Order by join date from sooner to later
-            {
-                _context.Entry(ug).Reference("role").Load();
-                _context.Entry(ug).Reference("User").Load();
-                if (ug.User.email != user.email)
-                {
-                    if(!ug.blocked || (ug.blocked && callerUser_role != role_group_normal))
-                    {
-                        _context.Entry(ug).Reference("blockedBy").Load();
-
-                        page.members.Add(new GroupMember {
-                            userName = ug.User.nickname,
-                            publicUserId = ug.User.publicId,
-                            role = ug.role.name,
-                            dateJoin = ug.dateJoin,
-                            dateRole = ug.dateRole,
-                            img = ug.User.profileImg,
-                            blocked = ug.blocked,
-                            blockedBy = ug.blockedBy != null ? ug.blockedBy.name : ""
-                        });
-                    }
-
-                }
-            }
-
-            return Ok(page);
+            return Ok(GroupPageManager.GetPage(user, group, _context));
         }
     }
 }
---
+++ Areas/GroupManage/Util/GroupPageManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Areas.GroupManage.Models;
+using API.Data;
+using API.Data.Models;
+
+namespace API.Areas.GroupManage.Util
+{
+    public static class GroupPageManager
+    {
+        public static GroupPage GetPage(User caller, Group group, ApplicationDBContext _context)
+        {
+            try
+            {
+                UserGroup callerInGroup = _context.UserGroup.Where(ug => ug.userId == caller.id).First();
+                _context.Entry(callerInGroup).Reference("role").Load();
+
+                string callerInGroup_role = callerInGroup.role.name;
+                string role_group_normal = _context.Role.Where(r => r.name == "GROUP_NORMAL").First().name;
+                string role_group_maker = _context.Role.Where(r => r.name == "GROUP_MAKER").First().name;
+                string role_group_admin = _context.Role.Where(r => r.name == "GROUP_ADMIN").First().name;
+
+                GroupPage page = new GroupPage();
+                page.groupName = group.name;
+                page.groupType = group.type;
+                page.role = callerInGroup_role;
+                page.bets = getBets(_context);
+                page.members = getMembers(callerInGroup_role, group, _context, role_group_normal);
+
+                return page;
+            }
+            catch (Exception)
+            {
+                return new GroupPage();
+            }
+
+        }
+
+        private static List<GroupBet> getBets(ApplicationDBContext _context)
+        {
+            return new List<GroupBet>{
+                new GroupBet { betName = "bet1", betBody = "betBody1"},
+                new GroupBet { betName = "bet2", betBody = "betBody2"},
+                new GroupBet { betName = "bet3", betBody = "betBody3"},
+                new GroupBet { betName = "bet4", betBody = "betBody4"},
+                new GroupBet { betName = "bet5", betBody = "betBody5"}
+            };
+        }
+
+        private static List<GroupMember> getMembers(string callerRoleInGroup, Group group, ApplicationDBContext _context, string roleGroup_normal)
+        {
+            List<GroupMember> members = new List<GroupMember>();
+            _context.Entry(group).Collection("users").Load();
+
+            group.users.ToList().ForEach(user =>
+            {
+                if(!user.blocked || (user.blocked && callerRoleInGroup != roleGroup_normal))
+                {
+                    _context.Entry(user).Reference("blockedBy").Load();
+                    _context.Entry(user).Reference("User").Load();
+
+                    members.Add(new GroupMember
+                    {
+                        userName = user.User.nickname,
+                        publicUserId = user.User.publicId,
+                        role = user.role.name,
+                        dateJoin = user.dateJoin,
+                        dateRole = user.dateRole,
+                        img = user.User.profileImg,
+                        blocked = user.blocked,
+                        blockedBy = user.blockedBy != null ? user.blockedBy.name : ""
+                    });
+                }
+            });
+
+            return members;
+        }
+    }
+}
