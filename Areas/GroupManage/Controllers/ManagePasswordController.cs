8de60157165370193e6e0fd9a0fc32c6f5af6f04
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-26 10:24:28 +0200

    Add managePassword in group

---
+++ Areas/GroupManage/Controllers/ManagePasswordController.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using API.Areas.GroupManage.Models;
+using API.Areas.GroupManage.Util;
+using API.Data;
+using API.Data.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.GroupManage.Controllers
+{
+    [Route("Group/[action]")]
+    [ApiController]
+    public class ManagePasswordController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public ManagePasswordController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ActionName("ManagePassword")]
+        public IActionResult managePassword([FromBody] ManagePassword order)
+        {
+            User user = TokenUserManager.getUserFromToken(HttpContext, _context); //The user who tries to make admin to another user
+            UserGroup ugCaller = new UserGroup();
+            Group group = new Group();
+
+            if(!UserInGroup.checkUserInGroup(user.id, ref group, order.name, ref ugCaller, _context))
+            {
+                return BadRequest(new { error = "" });
+            }
+            if(!hasPermissions(ugCaller, group, order.newPassword, order.oldPassword))
+            {
+                return BadRequest(new { error = "" });
+            }
+            if(group.password!=null && !PasswordHasher.areEquals(order.oldPassword, group.password))
+            {
+                return BadRequest(new { error = "" });
+            }
+
+            group.password = PasswordHasher.hashPassword(order.newPassword);
+            _context.Update(group);
+            _context.SaveChanges();
+            
+            return Ok(GroupPageManager.GetPage(user, group, _context));
+        }
+
+        private bool hasPermissions(UserGroup ugCaller, Group group, string newPassword, string oldPassword)
+        {
+            Role role_groupMaker = _context.Role.Where(r => r.name == "GROUP_MAKER").First();
+            bool role = ugCaller.role == role_groupMaker;
+            bool newPass = newPassword != null;
+            bool oldPass = oldPassword != null;
+            bool canPutPass = group.canPutPassword;
+            bool hasPassword = group.password != null;
+
+            if(!role || !newPass || !canPutPass)
+            {
+                return false;
+            }
+
+            if (  (oldPass && !hasPassword) || (!oldPass && hasPassword) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Areas/GroupManage/Models/ManagePassword.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Areas.GroupManage.Models
+{
+    public class ManagePassword
+    {
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string newPassword { get; set; }
+        public string oldPassword { get; set; }
+    }
+}
