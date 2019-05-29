492407cbbfdab3bfb0ec86afb6f1483c7fab029a
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-29 17:26:23 +0200

    Add leaveGroup function

---
+++ Areas/GroupManage/Controllers/LeaveGroupController.cs
@@ -0,0 +1,43 @@
+﻿using API.Areas.GroupManage.Util;
+using API.Data;
+using API.Data.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.GroupManage.Controllers
+{
+    [Route("Group/[action]")]
+    [ApiController]
+    public class LeaveGroupController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public LeaveGroupController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [ActionName("LeaveGroup")]
+        public IActionResult leaveGroup(string groupName)
+        {
+            User user = TokenUserManager.getUserFromToken(HttpContext, _context); //The user who tries to leave the group
+            UserGroup ugCaller = new UserGroup();
+            Group group = new Group();
+
+            if(!UserInGroup.checkUserInGroup(user.id, ref group, groupName, ref ugCaller, _context))
+            {
+                return BadRequest();
+            }
+            if (!QuitUserFromGroup.quitUser(ugCaller, _context))
+            {
+                return StatusCode(500);
+            }
+
+
+            return Ok(new { success="SuccesfullGroupLeave"});
+        }
+    }
+}
