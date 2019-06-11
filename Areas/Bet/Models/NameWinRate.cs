a1783387143baa09fff26999167a0dbfa44537f5
Javier Gómez Luzón <javo14795@gmail.com>
2019-06-12 01:21:15 +0200

    Add cancelRate on NameWinRate model

--- Areas/Bet/Models/NameWinRate.cs
+++ Areas/Bet/Models/NameWinRate.cs
@@ -9,15 +9,18 @@ namespace API.Areas.Bet.Models
             this.name = type.name;
             this.description = type.description;
             this.winRate = type.winRate;
+            this.cancelRate = type.winLoseCancel;
         }
         public NameWinRate(TypePay type)
         {
             this.name = type.name;
             this.description = type.description;
             this.winRate = type.winRate;
+            this.cancelRate = type.winLoseCancel;
         }
         public string name { get; set; }
         public string description { get; set; }
         public double winRate { get; set; }
+        public double cancelRate { get; set; }
     }
 }
