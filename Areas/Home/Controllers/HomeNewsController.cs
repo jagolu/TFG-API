b59773c2ea31c8da841d5d7f519da8d77369a4aa
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-10 17:01:47 +0200

    Add getnews controller

---
+++ Areas/Home/Controllers/HomeNewsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Areas.Home.Models;
+using API.Data;
+using API.Data.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Areas.Home.Controllers
+{
+    [Route("Home/[action]")]
+    [ApiController]
+    public class HomeNewsController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public HomeNewsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [ActionName("AuthHome")]
+        public IActionResult getAuth()
+        {
+            User user = TokenUserManager.getUserFromToken(HttpContext, _context);
+
+            try
+            {
+                List<New> news = _context.News.Where(n => n.userId == user.id || n.userId == null).OrderByDescending(nn => nn.date).ToList();
+                List<NewMessage> retMessage = addNews(news);
+
+                return Ok(retMessage);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+        [HttpGet]
+        [ActionName("StandHome")]
+        public IActionResult GetStand()
+        {
+            try
+            {
+                List<New> news = _context.News.Where(n => n.userId == null).OrderByDescending(nn => nn.date).ToList();
+                List<NewMessage> retMessage = addNews(news);
+
+                return Ok(retMessage);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+
+        private List<NewMessage> addNews(List<New> news)
+        {
+            List<NewMessage> retMessage = new List<NewMessage>();
+            news.ForEach(n => retMessage.Add(new NewMessage(n)));
+
+            return retMessage;
+        }
+    }
+}
--- Areas/Home/Models/NewMessage.cs
+++ Areas/Home/Models/NewMessage.cs
@@ -4,6 +4,12 @@ namespace API.Areas.Home.Models
 {
     public class NewMessage
     {
+        public NewMessage(Data.Models.New notice)
+        {
+            this.title = notice.title;
+            this.body = notice.message;
+            this.time = notice.date;
+        }
 
         public string title { get; set; }
 
