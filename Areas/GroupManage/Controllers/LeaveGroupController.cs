43b8fcad8ade7b99f142d84fd61395d5343a4750
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-10 16:36:26 +0200

    Fix leave group new

--- Areas/GroupManage/Controllers/LeaveGroupController.cs
+++ Areas/GroupManage/Controllers/LeaveGroupController.cs
@@ -4,6 +4,8 @@ using API.Data.Models;
 using API.Util;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace API.Areas.GroupManage.Controllers
 {
@@ -12,10 +14,12 @@ namespace API.Areas.GroupManage.Controllers
     public class LeaveGroupController : ControllerBase
     {
         private ApplicationDBContext _context;
+        private readonly IServiceScopeFactory scopeFactory;
 
-        public LeaveGroupController(ApplicationDBContext context)
+        public LeaveGroupController(ApplicationDBContext context, IServiceScopeFactory sf)
         {
             _context = context;
+            scopeFactory = sf;
         }
 
         [HttpGet]
@@ -36,7 +40,13 @@ namespace API.Areas.GroupManage.Controllers
                 return StatusCode(500);
             }
 
-            Common.Util.GroupNew.launch(user, group, Common.Models.TypeGroupNew.JOIN_LEFT, false, _context);
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+                var groupE = dbContext.Group.Where(g => g.name == groupName);
+
+                if(groupE.Count() == 1)  Common.Util.GroupNew.launch(user, group, Common.Models.TypeGroupNew.JOIN_LEFT, false, _context);
+            }
 
             return Ok(new { success="SuccesfullGroupLeave"});
         }
