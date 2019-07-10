1e00aea861210ed5a488d8a08347f96920edf21b
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-10 19:24:22 +0200

    Reset TypeGroupNew

--- Areas/Home/Models/TypeGroupNew.cs
+++ Areas/Home/Models/TypeGroupNew.cs
@@ -2,15 +2,15 @@
 {
     public enum TypeGroupNew
     {
-        BLOCK_USER_USER = 1,
-        BLOCK_USER_GROUP = 11,
-        JOIN_LEFT_GROUP = 2,
-        JOIN_LEFT_USER = 21,
-        MAKE_ADMIN_USER = 3,
-        MAKE_ADMIN_GROUP = 31,
-        MAKE_PRIVATE = 4,
-        REMOVE_GROUP = 5,
-        KICK_USER_USER = 6,
-        KICK_USER_GROUP = 61
+        BLOCK_USER_USER = 2001,
+        BLOCK_USER_GROUP = 2002,
+        JOIN_LEFT_GROUP = 3001,
+        JOIN_LEFT_USER = 3002,
+        MAKE_ADMIN_USER = 4001,
+        MAKE_ADMIN_GROUP = 4002,
+        MAKE_PRIVATE = 5001,
+        REMOVE_GROUP = 6001,
+        KICK_USER_USER = 7001,
+        KICK_USER_GROUP = 7002,
     }
 }
