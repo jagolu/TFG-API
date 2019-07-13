0625533aea2f9667b0bfca22c648f30e15798629
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-13 21:10:48 +0200

    Add ban group

---
+++ Areas/Admin/Models/BanGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Areas.Admin.Models
+{
+    public class BanGroup
+    {
+        [Required]
+        public string groupName { get; set; }
+
+        [Required]
+        public bool ban { get; set; }
+    }
+}
