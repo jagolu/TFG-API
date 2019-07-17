044ebc983f24cf7cf5c029b111e5d740521b0179
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-17 22:48:43 +0200

    Add OpenCloseDM

--- Util/EmailSender.cs
+++ Util/EmailSender.cs
@@ -63,6 +63,18 @@ namespace API.Util
             _client.Send(msg);
         }
 
+        public static void sendOpenCloseDMNotification(string email, string name, string dmTitle, bool open)
+        {
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(_configuration["Email:username"] + "@gmail.com");
+            msg.To.Add(email);
+            msg.Body = open ? getBodySendOpenDMNotification(name, dmTitle) : getBodySendCloseDMNotification(name, dmTitle);
+            msg.IsBodyHtml = true;
+            msg.Subject = open ? "Un administrador ha abierto de nuevo una conversación" : "Un administrador ha cerrado una conversación";
+
+            _client.Send(msg);
+        }
+
         private static void initializeClient()
         {
             _client = new SmtpClient("smtp.gmail.com");
@@ -123,6 +135,26 @@ namespace API.Util
             string body = "<html><head></head><body>";
             body += "<h1>Hola "+name+"</h1><br>";
             body += "<p>Uno de los administradores de la plataforma te ha contestado en la ";
+            body += "conversación sobre el tema " + dmTitle + "</p>";
+            body += "</body></html>";
+            return body;
+        }
+
+        private static String getBodySendOpenDMNotification(string name, string dmTitle)
+        {
+            string body = "<html><head></head><body>";
+            body += "<h1>Hola "+name+"</h1><br>";
+            body += "<p>Uno de los administradores de la plataforma ha abierto de nuevo la ";
+            body += "conversación sobre el tema " + dmTitle + "</p>";
+            body += "</body></html>";
+            return body;
+        }
+
+        private static String getBodySendCloseDMNotification(string name, string dmTitle)
+        {
+            string body = "<html><head></head><body>";
+            body += "<h1>Hola "+name+"</h1><br>";
+            body += "<p>Uno de los administradores de la plataforma ha cerrado la ";
             body += "conversación sobre el tema " + dmTitle + "</p>";
             body += "<p>Si tienes otro problema no dudes en abrir otra conversación.</p>";
             body += "</body></html>";
