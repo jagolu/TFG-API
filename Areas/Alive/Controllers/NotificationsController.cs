569bc62514b82abce29a2ef4579df62565c893f4
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-18 18:34:23 +0200

    Add getNotifications http request

---
+++ Areas/Alive/Controllers/NotificationsController.cs
@@ -0,0 +1,50 @@
+﻿using API.Areas.Alive.Models;
+using API.Data;
+using API.Data.Models;
+using API.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Areas.Alive.Controllers
+{
+    [Route("Alive/[action]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private ApplicationDBContext _context;
+
+        public NotificationsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [ActionName("NotificationsLogin")]
+        public IActionResult LoginNotifications()
+        {
+            User user = TokenUserManager.getUserFromToken(HttpContext, _context);
+            if (!user.open) return BadRequest(new { error = "YoureBanned" });
+
+            try
+            {
+                _context.Entry(user).Collection("notifications").Load();
+                List<NotificationMessage> notifications = new List<NotificationMessage>();
+
+                user.notifications.OrderBy(n => n.time).ToList().ForEach(n =>
+                {
+                    notifications.Add(new NotificationMessage { id = n.id.ToString(), message = n.message });
+                });
+
+                return Ok(notifications);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
