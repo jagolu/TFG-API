106c6b272bfbcf63bcc9d6fa9fec3a454a8a591c
Javier Gómez Luzón <javo14795@gmail.com>
2019-08-15 00:12:41 +0200

    Reorganize

--- Areas/Bet/Models/GroupBet.cs
+++ Areas/Bet/Models/GroupBet.cs
@@ -16,11 +16,11 @@ namespace API.Areas.Bet.Models
             this.bet = bet.id.ToString();
             this.competition = bet.MatchDay.Competition.name;
             this.betName = bet.MatchDay.HomeTeam.name + " vs " + bet.MatchDay.AwayTeam.name;
+            this.matchdayDate = bet.MatchDay.date;
             this.typeBet = new NameWinRate(bet.type);
             this.typePay = new NameWinRate(bet.typePay);
             this.minBet = bet.minBet;
             this.maxBet = bet.maxBet;
-            this.matchdayDate = bet.MatchDay.date;
             this.lastBetTime = bet.dateLastBet;
             if (Util.CheckBetType.isJackpot(bet, _context))
             {
@@ -36,14 +36,18 @@ namespace API.Areas.Bet.Models
                 this.fullTimeAwayGoals = bet.MatchDay.fullTimeAwayGoals;
             }
         }
-        public string bet { get; set; }
+        public string bet { get; set; } //id
+
+        // Match info
         public string competition { get; set; }
         public string betName { get; set; }
+        public DateTime matchdayDate { get; set; }
+
+        // Football bet info
         public NameWinRate typeBet { get; set; }
         public NameWinRate typePay { get; set; }
         public int minBet { get; set; }
         public int maxBet { get; set; }
-        public DateTime matchdayDate { get; set; }
         public DateTime lastBetTime { get; set; }
         public int? usersJoined { get; set; }
 
